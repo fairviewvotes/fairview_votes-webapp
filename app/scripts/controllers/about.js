9c8bbaddecc0b65537c5ba1750d8f80d76a8afcc
Saketram Durbha <durbhasaketram@gmail.com>
2018-03-09 13:55:17 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fairviewVotesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fairviewVotesApp
+ */
+angular.module('fairviewVotesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
